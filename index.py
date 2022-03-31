007fb14b9911fc9c85e090b5c1663eb4e99e3a9b
erickuo5124 <erickuo5124@gmail.com>
2022-03-31 12:19:55 +0800

    feat(index): remove slack bot

--- index.py
+++ index.py
@@ -19,9 +19,9 @@ def post():
   PAGE_ID = request.args.get('id')
   if PAGE_ID != '':
     msg, dc_msg = get_post_msg(PAGE_ID)
-    sl.post_slack(msg)
+    # sl.post_slack(msg)
     no.update_status(PAGE_ID)
-    no.create_page('c20f4fd6438f4c75aa3e06096b9c8b23')
+    # no.create_page('c20f4fd6438f4c75aa3e06096b9c8b23')
     dc.post_discord(dc_msg)
   return redirect(url_for('index'))
 
